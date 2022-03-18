eb7149a87ca305cc3828b04700352a3fbf257223
kareemmonsur <monsurkareem@gmail.com>
2022-03-18 20:19:57 +0100

    radio buttons added

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { TextField, Typography, Box } from '@mui/material'
-import { Button } from '@mui/material'
-import { Container } from '@mui/material'
-
-
+import { Button, Container, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@mui/material'
 
 
 function Create() {
@@ -18,7 +15,9 @@ function Create() {
           Create a new Note
       </Typography>
 
-      <Box noValidate autoComplete='off' component= 'form'>
+      <Box noValidate autoComplete='off' component= 'form'  sx={{
+        '& .MuiTextField-root': { m: 2},
+      }}>
         <TextField 
           label="NoteTitle" 
           variant="outlined" 
@@ -36,10 +35,21 @@ function Create() {
           rows={'10'}
         />
 
+      <FormControl>
+        <FormLabel id="demo-radio-buttons-group-label">Note Category</FormLabel>
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="female"
+          name="radio-buttons-group"
+        >
+          <FormControlLabel value="Money" control={<Radio />} label="Money" />
+          <FormControlLabel value="Todos" control={<Radio />} label="Todos" />
+          <FormControlLabel value="Reminder" control={<Radio />} label="Reminder" />
+          <FormControlLabel value="Work" control={<Radio />} label="Work" />
+        </RadioGroup>
+      </FormControl>
 
-        
-
-      </Box>
+    </Box>
 
       
       
