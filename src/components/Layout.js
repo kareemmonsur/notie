7b1f8cdf3e4caf1015bfdd6293f7c3fc2f2ff0d7
kareemmonsur <monsurkareem@gmail.com>
2022-03-21 21:38:33 +0100

    drawer added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,37 @@
+import { Typography, Drawer, Box} from '@mui/material';
 import React from 'react'
 
 function Layout({children}) {
+
+    const drawerWidth = 240;
   return (
     <div>
+        {/* App bar */}
+
+        {/* side bar */}
+        <Drawer
+        variant='permanent'
+        anchor='left'
+         sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: drawerWidth,
+              boxSizing: 'border-box',
+              
+            },
+         }}
+        >
+            <div>
+                <Typography variant='h5'>
+                    Notie
+                </Typography>
+            </div>
+
+        </Drawer>
+
+
+
         <div> 
             {children}
          </div>
